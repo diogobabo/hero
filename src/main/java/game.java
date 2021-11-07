8b542c2ec7b6eafae3463976a5ae2845c7cd61fd
Diogo Babo <diogo.babo@gmail.com>
2021-11-07 20:48:48 +0000

    cena 2

--- src/main/java/game.java
+++ src/main/java/game.java
@@ -8,17 +8,25 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class game {
-    TerminalSize terminalSize = new TerminalSize(40, 20);
-    DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-    Terminal terminal = terminalFactory.createTerminal();
-    Screen screen = new TerminalScreen(terminal);
 
     public game() throws IOException {
-    }
+
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        Screen screen = new TerminalScreen(terminal);
         screen.setCursorPosition(null);
         screen.startScreen();
         screen.doResizeIfNecessary();
         screen.clear();
         screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
+    }
+    private void draw() throws IOException{
+
+    }
+    public void run() throws IOException {
+        draw();
+    }
+
 }
